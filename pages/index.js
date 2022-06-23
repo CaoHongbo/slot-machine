6297e9732bece3362b30c3842117fdb52762208f
caohongbo <papa_100@163.com>
2022-06-23 11:56:16 +0800

    styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import "./index.module.sass";
+// import styles from "./index.module.sass";
 
 import BGImage from "../public/game_backdrop.jpg";
 import CanvasImage from "../public/game_canvas.png";
@@ -17,70 +17,87 @@ import icon8 from "../public/icon_8_dynamite.png";
 import icon9 from "../public/icon_9_skull.png";
 import icon10 from "../public/icon_10_sheriff.png";
 
-function Items() {
-  return (
-    <div className="items">
-      <div className="c1">
-        <Image src={icon1} width={50} height={50}></Image>
-        <Image src={icon2} width={50} height={50}></Image>
-        <Image src={icon3} width={50} height={50}></Image>
-        <Image src={icon4} width={50} height={50}></Image>
-        <Image src={icon5} width={50} height={50}></Image>
-        <Image src={icon6} width={50} height={50}></Image>
-        <Image src={icon7} width={50} height={50}></Image>
-        <Image src={icon8} width={50} height={50}></Image>
-        <Image src={icon9} width={50} height={50}></Image>
-        <Image src={icon10} width={50} height={50}></Image>
+const icons = [
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+  icon5,
+  icon6,
+  icon7,
+  icon8,
+  icon9,
+  icon10,
+];
+
+function getItems() {
+  const items = [];
+  for (let i = 0; i < 5; i++) {
+    items.push(
+      <div key={i + 1} className={`c${i + 1}`} style={{ float: "left" }}>
+        {icons.map((d, i) => {
+          return (
+            <div>
+              <Image src={d} key={i} width={119} height={119}></Image>
+            </div>
+          );
+        })}
       </div>
-      <div className="c2">
-        <Image src={icon1} width={50} height={50}></Image>
-        <Image src={icon2} width={50} height={50}></Image>
-        <Image src={icon3} width={50} height={50}></Image>
-        <Image src={icon4} width={50} height={50}></Image>
-        <Image src={icon5} width={50} height={50}></Image>
-        <Image src={icon6} width={50} height={50}></Image>
-        <Image src={icon7} width={50} height={50}></Image>
-        <Image src={icon8} width={50} height={50}></Image>
-        <Image src={icon9} width={50} height={50}></Image>
-        <Image src={icon10} width={50} height={50}></Image>
-      </div>
-      <div className="c3">
-        <Image src={icon1} width={50} height={50}></Image>
-        <Image src={icon2} width={50} height={50}></Image>
-        <Image src={icon3} width={50} height={50}></Image>
-        <Image src={icon4} width={50} height={50}></Image>
-        <Image src={icon5} width={50} height={50}></Image>
-        <Image src={icon6} width={50} height={50}></Image>
-        <Image src={icon7} width={50} height={50}></Image>
-        <Image src={icon8} width={50} height={50}></Image>
-        <Image src={icon9} width={50} height={50}></Image>
-        <Image src={icon10} width={50} height={50}></Image>
-      </div>
-      <div className="c4"></div>
-      <div className="c5"></div>
-    </div>
-  );
+    );
+  }
+
+  return items;
 }
 
 export default function Home() {
   return (
     <div className="container">
       <div style={{ position: "absolute" }}>
-        <Image src={BGImage} width={1920} height={1080}></Image>
+        <Image src={BGImage}></Image>
       </div>
 
       <div className="canvas" style={{ position: "absolute" }}>
         <Image src={CanvasImage}></Image>
       </div>
 
+      <div className="items">
+        <div className="itemsicon clearfix">{getItems()}</div>
+      </div>
+
       <div className="slot-machine" style={{ position: "absolute" }}>
         <Image src={CanvasOverImage}></Image>
-        <Items></Items>
       </div>
-      {/* 
-      <div className="item" style={{ position: "absolute" }}>
-        <Image src={CanvasOverImage}></Image>
-      </div> */}
+
+      <style jsx>{`
+        .items {
+          width: 100%;
+          height: 100%;
+          position: absolute;
+        }
+
+        .itemsicon {
+          width: 595px;
+          // height: 595px;
+          // background-color: red;
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          top: 150px;
+          margin: auto;
+          overflow: hidden;
+        }
+
+        .clearfix {
+          *zoom: 1;
+        }
+
+        .clearfix:after {
+          content: "";
+          display: block;
+          clear: both;
+        }
+      `}</style>
     </div>
   );
 }
